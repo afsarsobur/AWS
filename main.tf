c350308a5e24ae6717315c0d4ebdd62b54ed3442
afsarsobur <afsar.sobur1@gmail.com>
2025-09-17 16:53:30 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,24 +2,20 @@
 
 # Configure the AWS Provider
 provider "aws" {
-  region = "us-east-1" 
+  region = "us-east-1"
 }
 
 # Create the S3 bucket
 resource "aws_s3_bucket" "sensitive_data_bucket" {
-  bucket = "dyn-media-interview-task"  
-}
+  bucket = "dyn-media-int-task"
 
-# Configure a lifecycle rule to transition objects to Glacier Deep Archive after 90 days
+# Configure a lifecycle rule
 resource "aws_s3_bucket_lifecycle_configuration" "glacier_transition" {
-  bucket = aws_s3_bucket.sensitive_dyn-media-interview-task
+  bucket = aws_s3_bucket.sensitive_data_bucket.id
   rule {
-    id = "transition_to_glacier"
+    id = "Acchivetoglachier" # The lifecycle rule name you requested
     status = "Enabled"
-    
-    # Add an empty filter block to apply the rule to all objects
     filter {}
-    
     transition {
       days          = 90
       storage_class = "GLACIER_DEEP_ARCHIVE"
