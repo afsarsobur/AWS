727189d7d4ec6e6906afd462db44a8625e13516a
afsarsobur <afsar.sobur1@gmail.com>
2025-09-17 18:56:08 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,12 @@
 provider "aws" {
-  region = "us-east-1"  # specify your preferred region
+  region = "us-east-1"
 }
 
 resource "aws_s3_bucket" "my_bucket" {
-  bucket = "my-unique-bucket-name-12345"  # bucket names must be globally unique
+  bucket = "my-unique-bucket-name-12345"
+}
+
+resource "aws_s3_bucket_acl" "my_bucket_acl" {
+  bucket = aws_s3_bucket.my_bucket.id
   acl    = "private"
 }
