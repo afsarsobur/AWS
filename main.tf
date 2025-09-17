95f259868e3bce5354fb4b16ee9d8c2610311314
afsarsobur <afsar.sobur1@gmail.com>
2025-09-17 16:57:30 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,24 +1,22 @@
-# main.tf
+resource "aws_iam_policy" "s3_read_only" {
+  name        = "s3-read-only-access-policy"
+  description = "Provides read-only access to a specific S3 bucket"
 
-# Configure the AWS Provider
-provider "aws" {
-  region = "us-east-1"
-}
-
-# Create the S3 bucket
-resource "aws_s3_bucket" "sensitive_data_bucket" {
-  bucket = "dyn-media-int-taskkk"
-}
-# Configure a lifecycle rule
-resource "aws_s3_bucket_lifecycle_configuration" "glacier_transition" {
-  bucket = aws_s3_bucket.sensitive_data_bucket.id
-  rule {
-    id = "Acchivetoglachier" # The lifecycle rule name you requested
-    status = "Enabled"
-    filter {}
-    transition {
-      days          = 90
-      storage_class = "GLACIER_DEEP_ARCHIVE"
-    }
-  }
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid    = "AllowS3ReadOnly"
+        Effect = "Allow"
+        Action = [
+          "s3:GetObject",
+          "s3:ListBucket"
+        ]
+        Resource = [
+          "arn:aws:s3:::my-sensitive-data-bucket",
+          "arn:aws:s3:::my-sensitive-data-bucket/*"
+        ]
+      },
+    ]
+  })
 }
