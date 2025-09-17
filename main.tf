2f468e7dbb2f82a46d8692e278c52bd0cba56c10
afsarsobur <afsar.sobur1@gmail.com>
2025-09-17 18:28:04 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,59 +1,48 @@
-provider "aws" {
-  region = "us-east-1"
-}
+data "aws_iam_policy_document" "s3_read_only_policy_doc" {
+  statement {
+    sid    = "AllowS3ReadOnly"
+    effect = "Allow"
 
-# Use your existing role (replace with your role name)
-data "aws_iam_role" "existing" {
-  name = "BOB"
-}
-
-# S3 bucket
-resource "aws_s3_bucket" "archive" {
-  bucket = "my-simple-archive-bucket-2025565"
-}
-
-# IAM Policy (identity-based)
-resource "aws_iam_policy" "readonly" {
-  name = "s3-archive-readonly"
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect   = "Allow"
-        Action   = ["s3:ListBucket"]
-        Resource = aws_s3_bucket.archive.arn
-      },
-      {
-        Effect   = "Allow"
-        Action   = ["s3:GetObject"]
-        Resource = "${aws_s3_bucket.archive.arn}/*"
-      }
+    actions = [
+      "s3:GetObject",
+      "s3:ListBucket",
     ]
-  })
-}
 
-resource "aws_iam_role_policy_attachment" "attach" {
-  role       = data.aws_iam_role.existing.name
-  policy_arn = aws_iam_policy.readonly.arn
-}
-
-# Bucket Policy (resource-based)
-resource "aws_s3_bucket_policy" "archive_policy" {
-  bucket = aws_s3_bucket.archive.id
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Principal = {
-          AWS = data.aws_iam_role.existing.arn
-        }
-        Action   = ["s3:ListBucket", "s3:GetObject"]
-        Resource = [
-          aws_s3_bucket.archive.arn,
-          "${aws_s3_bucket.archive.arn}/*"
-        ]
-      }
+    resources = [
+      "arn:aws:s3:::my-sensitive-data-bucket111",
+      "arn:aws:s3:::my-sensitive-data-bucket111/*",
     ]
-  })
+  }
+}
+
+resource "aws_iam_policy" "s3_read_only_policy" {
+  name        = "S3ReadOnlyPolicy"
+  description = "Allows read-only access to my-sensitive-data-bucket111"
+  policy      = data.aws_iam_policy_document.s3_read_only_policy_doc.json
+}
+data "aws_iam_policy_document" "my_sensitive_data_bucket_policy_doc" {
+  statement {
+    sid    = "AllowUserReadOnly"
+    effect = "Allow"
+    
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::123456789012:user/data-analyst"]
+    }
+
+    actions = [
+      "s3:GetObject",
+      "s3:ListBucket",
+    ]
+
+    resources = [
+      "arn:aws:s3:::my-sensitive-data-bucket",
+      "arn:aws:s3:::my-sensitive-data-bucket/*",
+    ]
+  }
+}
+
+resource "aws_s3_bucket_policy" "my_sensitive_data_bucket_policy" {
+  bucket = "my-sensitive-data-bucket"
+  policy = data.aws_iam_policy_document.my_sensitive_data_bucket_policy_doc.json
 }
