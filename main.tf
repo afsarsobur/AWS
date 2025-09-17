0874fff1eb3d40209e71432cd849aeb648b7d699
afsarsobur <afsar.sobur1@gmail.com>
2025-09-17 16:29:04 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,29 +1,38 @@
-# main.tf
-
-# Configure the AWS Provider
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-
-provider "aws" {
-  region = "us-east-1"
-}
-
-# Generate a unique bucket name
-resource "random_pet" "bucket_name_prefix" {
-  length = 2
-}
-
-# Create the S3 bucket for sensitive data
-resource "aws_s3_bucket" "sensitive_data" {
-  bucket = "sensitive-customer-data-${random_pet.bucket_name_prefix.id}"
+resource "aws_iam_user" "readonly_user" {
+  name = "s3-readonly-user"
   tags = {
-    Name        = "Sensitive Customer Data"
-    Environment = "production"
+    "Project" = "S3-Security-Task"
   }
+}
+
+resource "aws_iam_policy" "s3_readonly_policy" {
+  name        = "s3-readonly-policy"
+  description = "Provides read-only access to a specific S3 bucket"
+
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Action" : [
+          "s3:GetObject",
+          "s3:ListBucket"
+        ],
+        "Resource" : [
+          "arn:aws:s3:::my-secure-customer-data-bucket",
+          "arn:aws:s3:::my-secure-customer-data-bucket/*"
+        ]
+      }
+    ]
+  })
+}
+
+resource "aws_iam_user_policy_attachment" "attach_policy" {
+  user       = aws_iam_user.readonly_user.name
+  policy_arn = aws_iam_policy.s3_readonly_policy.arn
+}
+
+resource "aws_s3_bucket" "secure_customer_data" {
+  bucket = "my-secure-customer-data-bucket"
+  acl    = "private"
 }
