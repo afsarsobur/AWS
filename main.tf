6a1636ab26403a0032ea564afa9291f014bfbff2
afsarsobur <afsar.sobur1@gmail.com>
2025-09-17 19:01:29 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,8 @@
-provider "aws" {
-  region = "us-east-1"
-}
+resource "aws_s3_bucket" "example" {
+  bucket = "my-tf-test-bucket"
 
-resource "aws_s3_bucket" "my_bucket" {
-  bucket = "my-unique-bucket-name-12345"
-}
-
-resource "aws_s3_bucket_acl" "my_bucket_acl" {
-  bucket = aws_s3_bucket.my_bucket.id
-  acl    = "private"
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
 }
